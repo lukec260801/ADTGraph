959b36a97422f369f09c13ed70c5873985906b70
Luke <luke.campbell.2019@uni.strath.ac.uk>
2023-11-01 14:27:26 +0000

    Finished methods

--- bostonMetro/src/bostonMetro/multiGraph.java
+++ bostonMetro/src/bostonMetro/multiGraph.java
@@ -5,18 +5,57 @@ import java.io.*;
 
 public abstract class multiGraph {
 	
+	//Global Variables
 	int n;
-	LinkedList<Integer> queue[];
-
-	public void search() {
-		
+	LinkedList<Integer> graph[];
+	
+	//Start search of graph
+	public LinkedList<Integer> search(int s, int e) {
+		int prev[] = solve(s);
+		return reconPath(s, e, prev);
 	}
 	
-	public void solve(int s, int e) {
+	//Start solve of graph
+	public int[] solve(int s) {
+		LinkedList<Integer> queue = new LinkedList<>();
+		queue.add(s);
 		
+		boolean visited[] = new boolean[n];
+		visited[s] = true;
+		
+		while (queue.size() != 0) {
+			
+		}
+		
+		int prev[] = new int[n];
+		
+		while (!queue.isEmpty()) {
+			int node = queue.poll();
+			Iterator<Integer> i = graph[node].listIterator();
+			while (i.hasNext()) {
+				int t = i.next();
+				if (!visited[t]) {
+					visited[t] = true;
+					queue.add(t);
+				}
+			}
+		}
+		
+		return prev;
 	}
 	
-	public void reconPath() {
+	//Reconstruct the path from end to start
+	public LinkedList<Integer> reconPath(int s, int e, int prev[]) {
+		LinkedList<Integer> path = new LinkedList<>();
 		
+		for(int at = e; at != 0; at = prev[at]) {
+			path.add(at);
+		}
+		
+		if (path.get(0).equals(s)) {
+			return path;
+		} else {
+			throw new IllegalArgumentException("List is Empty");
+		}
 	}
 }
